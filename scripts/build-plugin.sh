55cbb49923224863c34eae904f7b566b4226a9ff
emre.akkaya <emre.akkaya@agem.com.tr>
2016-06-16 18:48:45 +0300

    update build script

--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -5,6 +5,7 @@
 #
 # Generated files can be found under /tmp/lider-ahenk-usb-plugin/
 ###
+set -e
 
 pushd $(dirname $0) > /dev/null
 PRJ_ROOT_PATH=$(dirname $(pwd -P))
@@ -29,8 +30,8 @@ echo "Export path: $EXPORT_PATH"
 # Copy resulting files
 echo "Copying generated files to $EXPORT_PATH..."
 mkdir -p "$EXPORT_PATH"
-mv -f "$PRJ_ROOT_PATH"/usb*.deb "$EXPORT_PATH"
-mv -f "$PRJ_ROOT_PATH"/usb*.changes "$EXPORT_PATH"
+mv -f "$PRJ_ROOT_PATH"/*.deb "$EXPORT_PATH"
+mv -f "$PRJ_ROOT_PATH"/*.changes "$EXPORT_PATH"
 cp -rf "$PRJ_ROOT_PATH"/lider-usb/target/lider-*.jar "$EXPORT_PATH"
 cp -rf "$PRJ_ROOT_PATH"/lider-console-usb/target/lider-console-*.jar "$EXPORT_PATH"
 echo "Copied generated files."
