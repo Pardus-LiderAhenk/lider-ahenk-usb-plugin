4310eb9146438813c253620bc51792c8d2b13812
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-01-27 16:14:59 +0300

    fix 1.0.0-beta version

--- lider-console-usb/src/tr/org/liderahenk/usb/constants/UsbConstants.java
+++ lider-console-usb/src/tr/org/liderahenk/usb/constants/UsbConstants.java
@@ -4,7 +4,7 @@ public class UsbConstants {
 
 	public static final String PLUGIN_NAME = "usb";
 
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-beta";
 
 	public static final class PARAMETERS {
 		public static final String WEBCAM = "webcam";
