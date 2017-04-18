edadbbe5a2a36bfb30afe6e77115c3f3111da0f7
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-04-18 10:04:10 +0300

    policy

--- lider-console-usb/src/tr/org/liderahenk/usb/handlers/UsbProfileHandler.java
+++ lider-console-usb/src/tr/org/liderahenk/usb/handlers/UsbProfileHandler.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import tr.org.liderahenk.liderconsole.core.constants.LiderConstants;
 import tr.org.liderahenk.liderconsole.core.editorinput.ProfileEditorInput;
+import tr.org.liderahenk.liderconsole.core.handlers.LiderAbstractHandler;
 import tr.org.liderahenk.usb.constants.UsbConstants;
 import tr.org.liderahenk.usb.dialogs.UsbProfileDialog;
 import tr.org.liderahenk.usb.i18n.Messages;
@@ -22,29 +23,39 @@ import tr.org.liderahenk.usb.i18n.Messages;
  * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
  *
  */
-public class UsbProfileHandler extends AbstractHandler {
+public class UsbProfileHandler extends LiderAbstractHandler {
 
 	private Logger logger = LoggerFactory.getLogger(UsbProfileHandler.class);
 
-	public Object execute(ExecutionEvent event) throws ExecutionException {
+//	public Object execute(ExecutionEvent event) throws ExecutionException {
+//
+//		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+//		IWorkbenchPage page = window.getActivePage();
+//
+//		try {
+//			// Here we open default profile editor implementation so that all
+//			// profiles can be handled by Lider Console Core.
+//			// We also pass our profile dialog implementation as parameter to
+//			// allow the editor use it dynamically.
+//			page.openEditor(
+//					new ProfileEditorInput(Messages.getString("USB"), UsbConstants.PLUGIN_NAME,
+//							UsbConstants.PLUGIN_VERSION, new UsbProfileDialog()),
+//					LiderConstants.EDITORS.PROFILE_EDITOR);
+//		} catch (PartInitException e) {
+//			logger.error(e.getMessage(), e);
+//		}
+//
+//		return null;
+//	}
 
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		IWorkbenchPage page = window.getActivePage();
-
-		try {
-			// Here we open default profile editor implementation so that all
-			// profiles can be handled by Lider Console Core.
-			// We also pass our profile dialog implementation as parameter to
-			// allow the editor use it dynamically.
-			page.openEditor(
-					new ProfileEditorInput(Messages.getString("USB"), UsbConstants.PLUGIN_NAME,
-							UsbConstants.PLUGIN_VERSION, new UsbProfileDialog()),
-					LiderConstants.EDITORS.PROFILE_EDITOR);
-		} catch (PartInitException e) {
-			logger.error(e.getMessage(), e);
-		}
-
-		return null;
+	
+	@Override
+	public ProfileEditorInput getEditorInput() {
+		// TODO Auto-generated method stub
+		return new ProfileEditorInput(Messages.getString("USB"), UsbConstants.PLUGIN_NAME,
+				UsbConstants.PLUGIN_VERSION, new UsbProfileDialog());
 	}
-
+	
+	
+	
 }
