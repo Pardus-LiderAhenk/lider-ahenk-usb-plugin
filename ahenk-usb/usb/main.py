45dc63116faf854baa62d4add155f7dfbfead7bb
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2018-01-24 16:44:39 +0300

    version name fix: 1.1

--- ahenk-usb/usb/main.py
+++ ahenk-usb/usb/main.py
@@ -5,7 +5,7 @@
 def info():
     inf = dict()
     inf['name'] = 'usb'
-    inf['version'] = '1.0.0'
+    inf['version'] = '1.1'
     inf['support'] = 'debian'
     inf['description'] = 'USB plugin provides to managing usb devices like printer, mouse etc.'
     inf['task'] = True
